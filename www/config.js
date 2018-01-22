fac47debbab8fa2f8d95a821246e8bedfc2d4302
gmx <moethetster@gmail.com>
2018-01-23 00:35:19 +0630

    fix endpoint

--- www/config.js
+++ www/config.js
@@ -7,25 +7,25 @@ var config = {
 
 	'endPoints': {
 
-		'groups': 'http://192.168.43.220:3000/groups',
-		'items' : 'http://192.168.43.220:3000/items',
+		'groups': 'http://192.168.1.5:3000/groups',
+		'items' : 'http://192.168.1.5:3000/items',
 		/* 'users' : 'http://localhost:8000/server.php?f=users.json',*/
 
 		'auth': {
 			'method': 'POST',
-			'url': 'http://192.168.43.220:3000/auth'
+			'url': 'http://192.168.1.5:3000/auth'
 		},
 
 		'order' : {
 
-			'url'   : 'http://192.168.43.220:3000/order',
+			'url'   : 'http://192.168.1.5:3000/order',
 			'method': 'POST'	
 
 		},
 
 		'logout': {
 			'method': 'POST',
-			'url': 'http://192.168.43.220:3000/auth/logout'
+			'url': 'http://192.168.1.5:3000/auth/logout'
 		}
 	}		
 
